e1da4548107a671c9f02cab90591516cc638bdfb
Leonardo Maldonado <leonardomso11@gmail.com>
2018-02-28 11:34:46 -0300

    Starting the menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let icon = document.querySelector('#bars');
-let navigation = document.querySelector('.navigation');
+let navigation = document.querySelector('.menu__navigation');
 
 icon.addEventListener('click', () => {
     navigation.setAttribute('style', 'justify-content: flex-end;');
